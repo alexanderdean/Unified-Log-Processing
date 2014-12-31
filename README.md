f0a1f2b3ad1bb60ccd7bbcdaaecdc1ab9f1d85c2
Alex Dean <alex@snowplowanalytics.com>
2014-12-31 19:37:33 +0200

    Added annotations

--- README.md
+++ README.md
@@ -1,19 +1,21 @@
 Unified Log Processing
 ======================
 
-Supporting material (code, schemas etc) for Unified Log Processing, authored by Alexander Dean and published by Manning Publications.
+Supporting material (code, schemas etc) for **[Unified Log Processing] [ulp]**, authored by Alexander Dean and published by Manning Publications.
 
 ## Quickstart
 
 Assuming git, **[Vagrant] [vagrant-install]** and **[VirtualBox] [virtualbox-install]** installed:
 
 ```bash
- host$ git clone https://github.com/snowplow/snowplow.git
+ host$ git clone https://github.com/alexanderdean/Unified-Log-Processing.git
  host$ cd snowplow
  host$ vagrant up && vagrant ssh
 guest$ cd /vagrant/ch02/2.1/hellocalculator
 guest$ gradle test
 ```
+
+[ulp]: http://manning.com/dean/
 
 [vagrant-install]: http://docs.vagrantup.com/v2/installation/index.html
 [virtualbox-install]: https://www.virtualbox.org/wiki/Downloads
--- ch0x/x.1/HelloCalculator.java
+++ ch0x/x.1/HelloCalculator.java
@@ -7,22 +7,22 @@ public class HelloCalculator {
   public static void main(String[] args) {
     if (args.length < 2) {
       String err = "too few inputs (" + args.length + ")";
-      throw new IllegalArgumentException(err);
+      throw new IllegalArgumentException(err);                     // a
     } else {
       try {
         Integer sum = sum(args);
         System.out.println("SUM: " + sum);
-      } catch (NumberFormatException nfe) {
+      } catch (NumberFormatException nfe) {                        // b
         String err = "not all inputs parseable to Integers";
-        throw new IllegalArgumentException(err);
+        throw new IllegalArgumentException(err);                   // a
       }
     }
   }
 
-  static Integer sum(String[] args) throws NumberFormatException {
+  static Integer sum(String[] args) throws NumberFormatException { // c
     return Arrays.asList(args)
       .stream()
-      .mapToInt(str -> Integer.parseInt(str))
+      .mapToInt(str -> Integer.parseInt(str))                      // d
       .sum();
   }
 }
