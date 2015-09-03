e758094adb6c4be292f6716f00b738ec752ba49a
Alex Dean <alex@snowplowanalytics.com>
2015-09-03 23:53:36 +0100

    Updated

--- ch11/11.2/aow-lambda/src/main/scala/aowlambda/Writer.scala
+++ ch11/11.2/aow-lambda/src/main/scala/aowlambda/Writer.scala
@@ -4,11 +4,9 @@ import awscala._, dynamodbv2._
 
 object Writer {
 
-  private val table = {
-    val ddb = DynamoDB.at(Region.US_EAST_1)
-    ddb.table("aow-lambda").getOrElse(throw new
-      RuntimeException("Couldn't get table"))
-  }
+  private implicit val ddb = DynamoDB.at(Region.US_EAST_1)
+  private val table = ddb.table("aow-lambda").getOrElse(throw new
+    RuntimeException("Couldn't get table"))
 
   def write(row: Row) {
 
