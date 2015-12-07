5a5890e27cf6e8b54977a67772facd5c5cb243f7
Alex Dean <alex@snowplowanalytics.com>
2015-12-07 01:08:25 +0000

    Wip

--- ch03/3.3/nile/src/main/java/nile/StreamApp.java
+++ ch03/3.3/nile/src/main/java/nile/StreamApp.java
@@ -12,17 +12,17 @@ public class StreamApp {
   public static void main(String[] args) throws IOException {
     String consumerServers = args[0];
     String producerServers = args[1];
-    String groupId = args[2];
-    String inTopic = args[3];
-    String goodTopic = args[4];
-    String badTopic = args[5];
-    LookupService maxmind = new LookupService(args[6],
-      LookupService.GEOIP_MEMORY_CACHE);
+    String groupId         = args[2];
+    String inTopic         = args[3];
+    String goodTopic       = args[4];
+    //String badTopic        = args[5];
+    //LookupService maxmind  = new LookupService(args[6],
+    //  LookupService.GEOIP_MEMORY_CACHE);
 
     Consumer consumer = new Consumer(consumerServers, groupId, inTopic);
-    // PassthruProducer producer = new PassthruProducer(producerServers, goodTopic);
-    FullProducer producer = new FullProducer(
-      producerServers, goodTopic, badTopic, maxmind);
+    PassthruProducer producer = new PassthruProducer(producerServers, goodTopic);
+    //FullProducer producer = new FullProducer(
+    //  producerServers, goodTopic, badTopic, maxmind);
     consumer.run(producer);
   }
 }
