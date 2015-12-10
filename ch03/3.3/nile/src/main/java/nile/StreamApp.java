c789cc9d7a88f1a3ba37100197251e80d640f94a
Alex Dean <alex@snowplowanalytics.com>
2015-12-10 23:32:27 +0000

    Fixed up

--- ch03/3.3/nile/src/main/java/nile/StreamApp.java
+++ ch03/3.3/nile/src/main/java/nile/StreamApp.java
@@ -3,15 +3,14 @@ package nile;
 public class StreamApp {
 
   public static void main(String[] args){
-    String consumerServers = args[0];
-    String producerServers = args[1];
-    String groupId         = args[2];
-    String inTopic         = args[3];
-    String goodTopic       = args[4];
+    String servers   = args[0];
+    String groupId   = args[1];
+    String inTopic   = args[2];
+    String goodTopic = args[3];
 
-    Consumer consumer = new Consumer(consumerServers, groupId, inTopic);
+    Consumer consumer = new Consumer(servers, groupId, inTopic);
     PassthruProducer producer = new PassthruProducer(
-      producerServers, goodTopic);
+      servers, goodTopic);
     consumer.run(producer);
   }
 }
--- ch03/3.4/nile/src/main/java/nile/StreamApp.java
+++ ch03/3.4/nile/src/main/java/nile/StreamApp.java
@@ -7,19 +7,18 @@ import com.maxmind.geoip.LookupService;
 public class StreamApp {
 
   public static void main(String[] args) throws IOException {       // a
-    String consumerServers = args[0];
-    String producerServers = args[1];
-    String groupId         = args[2];
-    String inTopic         = args[3];
-    String goodTopic       = args[4];
-    String badTopic        = args[5];                               // b
-    String maxmindFile     = args[6];                               // b
+    String servers     = args[0];
+    String groupId     = args[1];
+    String inTopic     = args[2];
+    String goodTopic   = args[3];
+    String badTopic    = args[4];                                   // b
+    String maxmindFile = args[5];                                   // b
 
-    Consumer consumer = new Consumer(consumerServers, groupId, inTopic);
+    Consumer consumer = new Consumer(servers, groupId, inTopic);
     LookupService maxmind  = new LookupService(maxmindFile,
       LookupService.GEOIP_MEMORY_CACHE);                            // c
     FullProducer producer = new FullProducer(
-      producerServers, goodTopic, badTopic, maxmind);               // d
+      servers, goodTopic, badTopic, maxmind);                       // d
     consumer.run(producer);
   }
 }
