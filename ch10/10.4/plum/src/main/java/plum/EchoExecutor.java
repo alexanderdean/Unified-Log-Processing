7928cd61e1b39dd00f9817773608685807dc11c1
Alex Dean <alex@snowplowanalytics.com>
2016-04-25 01:09:26 +0100

    Tweaks

--- ch10/10.4/plum/src/main/java/plum/EchoExecutor.java
+++ ch10/10.4/plum/src/main/java/plum/EchoExecutor.java
@@ -9,7 +9,7 @@ import org.apache.avro.io.*;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.specific.SpecificDatumReader;
 
-import plum.avro.Alert;
+import plum.avro.Alert;                                                // a
 
 public class EchoExecutor implements IExecutor {
 
@@ -18,7 +18,7 @@ public class EchoExecutor implements IExecutor {
 
   private static Schema schema;
   static {
-    try {
+    try {                                                              // b
       schema = new Schema.Parser()
         .parse(EchoExecutor.class.getResourceAsStream("/avro/alert.avsc"));
     } catch (IOException ioe) {
@@ -41,9 +41,9 @@ public class EchoExecutor implements IExecutor {
     try {
       Decoder decoder = DecoderFactory.get().jsonDecoder(schema, din);
       DatumReader<Alert> reader = new SpecificDatumReader<Alert>(schema);
-      Alert alert = reader.read(null, decoder);
+      Alert alert = reader.read(null, decoder);                        // c
       System.out.println("Alert " + alert.recipient.name + " about " +
-        alert.notification.summary);
+        alert.notification.summary);                                   // d
     } catch (IOException | AvroTypeException e) {
       System.out.println("Error executing command:" + e.getMessage());
     }
