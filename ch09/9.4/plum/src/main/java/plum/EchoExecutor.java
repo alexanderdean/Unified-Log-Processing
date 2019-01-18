cbbc0df3d35b6af4d17046d3ebdbdef9327ff9a5
val <valentin.crettaz@consulthys.com>
2019-01-18 07:20:57 +0100

    Updates to chapter 9

--- ch09/9.4/plum/src/main/java/plum/EchoExecutor.java
+++ ch09/9.4/plum/src/main/java/plum/EchoExecutor.java
@@ -9,16 +9,16 @@ import org.apache.avro.io.*;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.specific.SpecificDatumReader;
 
-import plum.avro.Alert;
+import plum.avro.Alert;                                                // a
 
-public class FullExecutor implements IExecutor {
+public class EchoExecutor implements IExecutor {
 
   private final KafkaProducer<String, String> producer;
   private final String eventsTopic;
 
   private static Schema schema;
   static {
-    try {
+    try {                                                              // b
       schema = new Schema.Parser()
         .parse(EchoExecutor.class.getResourceAsStream("/avro/alert.avsc"));
     } catch (IOException ioe) {
@@ -26,10 +26,9 @@ public class FullExecutor implements IExecutor {
     }
   }
 
-  public FullExecutor(String servers, String eventsTopic) {
+  public EchoExecutor(String servers, String eventsTopic) {
 
-    this.producer = new KafkaProducer(
-      IExecutor.createConfig(servers));
+    this.producer = new KafkaProducer(IExecutor.createConfig(servers));
     this.eventsTopic = eventsTopic;
   }
 
@@ -41,10 +40,9 @@ public class FullExecutor implements IExecutor {
     try {
       Decoder decoder = DecoderFactory.get().jsonDecoder(schema, din);
       DatumReader<Alert> reader = new SpecificDatumReader<Alert>(schema);
-      Alert alert = reader.read(null, decoder);
-      Emailer.send(alert);                                            // a
-      IExecutor.write(this.producer, this.eventsTopic,
-        "{ \"event\": \"email_sent\" }");                             // b
+      Alert alert = reader.read(null, decoder);                        // c
+      System.out.println("Alert " + alert.recipient.name + " about " +
+        alert.notification.summary);                                   // d
     } catch (IOException | AvroTypeException e) {
       System.out.println("Error executing command:" + e.getMessage());
     }
--- ch09/9.4/plum/src/main/java/plum/Emailer.java
+++ ch09/9.4/plum/src/main/java/plum/Emailer.java
@@ -7,9 +7,9 @@ import plum.avro.Alert;
 public final class Emailer {
 
   static final String MAILGUN_KEY =
-    "key-bd3c7d427836872bf02eb80b4b6a62fe";                                                  // a
+    "b5ca946169cf5e9e3da913adb5164587-059e099e-b8a64376";                                    // a
   static final String MAILGUN_SANDBOX =
-    "sandbox26321840e2114f06be28daad732d9795.mailgun.org";                                   // b
+    "sandbox64e16a38b40342f4a81a723f09f2ee85.mailgun.org";                                   // b
 
   private static final Configuration configuration = new Configuration()
     .domain(MAILGUN_SANDBOX)
@@ -17,11 +17,12 @@ public final class Emailer {
     .from("Test account", "postmaster@" + MAILGUN_SANDBOX);
 
   public static void send(Alert alert) {                                
-    MailBuilder.using(configuration)
+    Response resp = Mail.using(configuration)
       .to(alert.recipient.email.toString())
       .subject(alert.notification.summary.toString())
       .text(alert.notification.detail.toString())
       .build()
       .send();
+    System.out.println(resp.responseMessage());
   }
 }
